35f019994453c217ff48dae6ff9c83cd47f2885e
Justin Cheng <jucheng.ca@gmail.com>
2022-11-03 19:41:01 -0700

    Complete sprint one

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.counter = 0;
+    this.storage = {};
+  }
+
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+
+  pop() {
+    var lastValue = this.storage[this.counter - 1];
+    delete this.storage[this.counter - 1];
+    if (this.counter > 0) {
+      this.counter--;
+    }
+    return lastValue;
+  }
+
+  size() {
+    return this.counter;
   }
 
 }
