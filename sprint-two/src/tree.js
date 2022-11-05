9b1e19a7eca1cc866e4304d5b7da43ab1981aab8
SeijDeLeon <sdeleon@berkeley.edu>
2022-11-05 13:16:36 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];// fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,9 +12,45 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newChild = {children: [], addChild: treeMethods.addChild, contains: treeMethods.contains, value: value};
+  this.children.push(newChild);
+  /*
+  var index = this.children.length;
+  if (index === 0) {
+
+    //this.children[index] = {children: [], addChild: treeMethods.addChild};
+    //this.children[index].value = value;
+  } else {
+    this.children[index - 1].value = value;
+  }
+  console.log(this);
+  */
 };
 
 treeMethods.contains = function(target) {
+  var counter = 0;
+
+  var findTarget = function() {
+    counter += this.children.length;
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].value === target) {
+        return true;
+      }
+      if (this.children[i].children.length > 0) {
+        if (findTarget.call(this.children[i], target) === true) {
+          return true;
+        }
+      }
+      counter--;
+    }
+    if (counter === 0) { //if we're at the first child array
+      return false;
+    }
+  };
+
+  var foundTarget = findTarget.call(this, target);
+  return foundTarget;
+
 };
 
 
@@ -21,3 +58,5 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//tree.children[0].value).to.equal(5);
