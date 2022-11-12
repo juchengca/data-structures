120ef423556d82124db699c308e9c6e573b3ff81
SeijDeLeon <sdeleon@berkeley.edu>
2022-11-12 10:34:54 -0800

    Complete Hash Table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,13 +6,32 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  //console.log('entered insert');
+  if (k === 'val2') {
+    //debugger
+  }
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  if (this.retrieve(k) !== undefined) {
-    var bucket = this.retrieve(index);
-    var item = [k, v];
-    bucket.push(item);
-    this._storage.set(index, bucket);
+  // If the key already has a value then either overwrite or add
+  if (this._storage.get(index) !== undefined) {
+    var bucket = this._storage.get(index);
+    var flag = false;
+    for (var i = 0; i < bucket.length; i++) {
+      // if new key and old key match, overwrite
+      if (k === bucket[i][0]) {
+        bucket[i][1] = v;
+        this._storage.set(index, bucket);
+        flag = true;
+      }
+    }
+    // if flag is false then key was not found; add new key value pair to array
+    if (flag === false) {
+      var item = [k, v];
+      bucket.push(item);
+      this._storage.set(index, bucket);
+      return;
+    }
+    // if key is not found, create new array and insert to storage
   } else {
     var bucket = [];
     var item = [k, v];
@@ -24,8 +43,12 @@ HashTable.prototype.insert = function(k, v) {
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var values = this._storage.get(index);
-  console.log(values);
+  if (values === undefined) {
+    return undefined;
+  }
   for (var i = 0; i < values.length; i++) {
+    console.log('in the loop in retrieve');
+    console.log(values);
     if (k === values[i][0]) {
       return values[i][1];
     }
@@ -37,10 +60,20 @@ HashTable.prototype.remove = function(k) {
   this._storage.set(index, undefined);
 };
 
-
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
 
+/*
+var v1 = 'val1';
+var v2 = 'val2';
+var oldHashFunction = window.getIndexBelowMaxForKey;
+window.getIndexBelowMaxForKey = function() { return 0; };
+hashTable.insert(v1, v1);
+hashTable.insert(v2, v2);
+expect(hashTable.retrieve(v1)).to.equal(v1);
+expect(hashTable.retrieve(v2)).to.equal(v2);
+window.getIndexBelowMaxForKey = oldHashFunction;
+
+*/
